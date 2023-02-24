863c3b6db8751b7ad40a377e8f58c0005531fa74
skumar <ch@ng3m3>
2023-02-24 11:20:22 -0600

    First React App: Task Tracker, initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+import Header from './componenets/Header';
+import Tasks from './componenets/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dr. Appointment',
+      day: 'Feb 25 at 2:00pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Project Meeting',
+      day: 'Feb 26 at 1:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Project Design Review',
+      day: 'Feb 27 at 11:00am',
+      reminder: true,
+    },
+  ]);
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log('deleted', id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'You are all taken care off'}
     </div>
   );
 }
 
 export default App;
+
+/*
+1. We can only return one html parent element from the function
+2. we can return <> empty if we don't want any parent html element
+
+*/
